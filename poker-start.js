51518a98c73499a77b01fb3a507fe11f9a1db8be
Joey Byrne <Joey@Joeys-MacBook-Pro.local>
2016-02-05 14:50:30 -0800

    create new file for dealing functionality. defined values and suits of cards

---
+++ poker-start.js
@@ -0,0 +1,23 @@
+var cardSuit = [ 'S', 'H', 'D', 'C'];
+var cardValue = [ '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' ];
+
+function newDeck(cardSuit, cardValue) {
+
+}
+
+
+function dealCard() {
+
+}
+
+
+function dealCards(deck) {
+  var deck = deck;
+  var hand = []
+
+  for (var i = 0; i < 5; i++) {
+    var cardValueRemaining = deck.cardValue
+    deck.cardSuit
+  }
+
+} 
