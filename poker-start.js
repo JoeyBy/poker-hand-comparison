107a8f08b2c38d99291bd47989b1bdd3be3c2a5c
Joey Byrne <Joey@Joeys-MacBook-Pro.local>
2016-02-05 15:49:52 -0800

    cards deal into hand and are removed from original deck once they are.

--- poker-start.js
+++ poker-start.js
@@ -1,9 +1,8 @@
 var cardSuit = [ 'S', 'H', 'D', 'C'];
 var cardValue = [ '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' ];
+var deck = []
 
 function newDeck(cardSuit, cardValue) {
-  var deck = []
-
 // creates a new deck with suit and value pairs. pushes the pairs to the deck array as they are paired up. 
   for (var i = 0; i < cardSuit.length; i++) {
     var suit = cardSuit[i]
@@ -17,25 +16,23 @@ function newDeck(cardSuit, cardValue) {
   return deck;
 }
 
-
-function dealCard(deck) {
-  var newDeck = [];
-
-
+function randomNumber(deck) {
+  return Math.floor(Math.random() * (deck.length - 1 + 1)) + 1 
 }
 
+function dealHand(deck) {
+  var hand = [];
 
-function dealCards(deck) {
-  var deck = deck;
-  var hand = []
-
-  for (var i = 0; i < 5; i++) {
-    var cardValueRemaining = deck.cardValue
-    deck.cardSuit
+  for (var i = 0; i < 5; i++ ) {
+    var cardNumber = randomNumber(deck);
+    hand.push(deck[cardNumber])
+    deck.splice(deck[cardNumber], 1)
   }
-
-} 
+  console.log(deck.length)
+  console.log(hand)
+  return hand;
+}
 
 $(function () {
-  newDeck(cardSuit, cardValue);
+  dealHand(newDeck(cardSuit, cardValue));
 })
